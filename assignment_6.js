bf8246aee25089ec8cdb6d861d06c4b3cd5de37b
Jimmaely <83773908+Jimmaely@users.noreply.github.com>
2021-05-17 22:25:46 -0400

    Update assignment_6.js

--- assignment_6.js
+++ assignment_6.js
@@ -1,1 +1,9 @@
-var array = [Retta, Vern, Marica, Cyrus, Linette, Curt, Nichole, Jesica, Wynell, MIchaele];
+function longest_name(str_ara) {
+  var max = str_ara[0].length;
+  str_ara.map(v => max = Math.max(max, v.length));
+  result = str_ara.filter(v => v.length == max);
+  return result;
+}
+
+console.log(longest_name(['Retta', 'Vern', 'Marica',
+                                          'Cyrus','Linette','Curt','Nichole','Jesica','Wynell','Michaele']))
